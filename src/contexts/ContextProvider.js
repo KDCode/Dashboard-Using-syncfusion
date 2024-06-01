d0ff0eb35e7d31daebbb043d39112ab2a50fd62b
Karandeep Singh <karandeepsingh15102000@gmail.com>
2024-06-01 15:55:30 +0530

    context api Set up and Sidebar done

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,21 @@
+import React, { Children, createContext, useContext, useState } from "react";
+
+const StateContext = createContext();
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
+
+export const ContextProvider = ({ children }) => {
+  const [activeMenu, setACtiveMenu] = useState(true);
+
+  return (
+    <StateContext.Provider value={{ activeMenu }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
